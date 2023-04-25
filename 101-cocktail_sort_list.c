1442ac73ac6fe15c5bcd22129ec99f66aca1ec06
nebihadilsebo <shegolenur@hotmail.com>
2023-04-25 07:59:57 -0700

    sorting_algorithms

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,4 +1,4 @@
-#include"sort.h"
+#include "sort.h"
 #include <stdio.h>
 
 /**
@@ -10,56 +10,55 @@
  */
 void swap(listint_t **list, listint_t *node)
 {
-    listint_t *prev = node->prev, *next = node->next;
+	listint_t *prev = node->prev, *next = node->next;
 
-    if (prev)
-        prev->next = next;
-    else
-        *list = next;
-    next->prev = prev;
-    node->prev = next;
-    node->next = next->next;
-    next->next = node;
-    if (node->next)
-        node->next->prev = node;
-    print_list(*list);
+	if (prev)
+		prev->next = next;
+	else
+		*list = next;
+	next->prev = prev;
+	node->prev = next;
+	node->next = next->next;
+	next->next = node;
+	if (node->next)
+		node->next->prev = node;
+	print_list(*list);
 }
 
 /**
  * cocktail_sort_list - sorts a doubly linked list of integers
- *                       in ascending order using Cocktail shaker sort
- * @list: pointer to pointer to the head of the list
+ * in ascending order using cocktail shaker sort
+ * @list:pointer to pointer to the head of the list
  *
  * Return: void
  */
 void cocktail_sort_list(listint_t **list)
 {
-    int swapped;
-    listint_t *node = *list;
+	int swapped;
+	listint_t *node = *list;
 
-    if (node == NULL || node->next == NULL)
-        return;
-    do {
-        swapped = 0;
-        while (node->next != NULL) {
-            if (node->n > node->next->n) {
-                swap(list, node);
-                swapped = 1;
-            } else {
-                node = node->next;
-            }
-        }
-        if (!swapped)
-            break;
-        swapped = 0;
-        while (node->prev != NULL) {
-            if (node->n < node->prev->n) {
-                swap(list, node->prev);
-                swapped = 1;
-            } else {
-                node = node->prev;
-            }
-        }
-    } while (swapped);
-}                                                                                                                
-                    
+	if (node == NULL || node->next == NULL)
+		return;
+	do {
+		swapped = 0;
+		while (node->next != NULL) {
+			if (node->n > node->next->n) {
+				swap(list, node);
+				swapped = 1;
+			} else {
+				node = node->next;
+			}
+		}
+		if (!swapped)
+			break;
+		swapped = 0;
+		while (node->prev != NULL) {
+			if (node->n < node->prev->n) {
+				swap(list, node->prev);
+				swapped = 1;
+			} else {
+		       node = node->prev;
+		       }
+		}
+	} while (swapped);
+}
